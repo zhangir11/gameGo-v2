9a926748355d963f83d5f95a8da4c4fb4c796a7a
Arman123 <Arman123>
2022-02-14 21:10:20 +0600

    add base logic

---
+++ go.mod
@@ -0,0 +1,35 @@
+module rpg
+
+go 1.17
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/gorilla/websocket v1.4.2
+	github.com/hajimehoshi/ebiten/v2 v2.2.4
+	github.com/satori/go.uuid v1.2.0
+)
+
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210727001814-0db043d8d5be // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
+	golang.org/x/mobile v0.0.0-20210902104108-5d9a33257ab5 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
+	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- main.go
+++ main.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"log"
+	"rpg/game"
+	"sort"
+	"strconv"
 
 	_ "image/png"
 
+	"github.com/gorilla/websocket"
 	ebiten "github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+var world game.World
+var frame int
+var img *ebiten.Image
+var c *websocket.Conn
+
+func init() {
+	world = game.World{
+		IsServer: false,
+		Units:    game.Units{},
+	}
+}
 
 // Game implements ebiten.Game interface.
 type Game struct{}
@@ -16,16 +32,94 @@ type Game struct{}
 // Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
 	// Write your game's logical update.
+	if ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		c.WriteJSON(
+			game.Event{
+				Type: game.EventTypeMove,
+				Data: game.EventMove{
+					UnitID:    world.MyID,
+					Direction: game.DirectionLeft,
+				},
+			})
+
+		return nil
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyD) || ebiten.IsKeyPressed(ebiten.KeyRight) {
+		c.WriteJSON(
+			game.Event{
+				Type: game.EventTypeMove,
+				Data: game.EventMove{
+					UnitID:    world.MyID,
+					Direction: game.DirectionRight,
+				},
+			})
+
+		return nil
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyW) || ebiten.IsKeyPressed(ebiten.KeyUp) {
+		c.WriteJSON(
+			game.Event{
+				Type: game.EventTypeMove,
+				Data: game.EventMove{
+					UnitID:    world.MyID,
+					Direction: game.DirectionUp,
+				},
+			})
+
+		return nil
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyS) || ebiten.IsKeyPressed(ebiten.KeyDown) {
+		c.WriteJSON(
+			game.Event{
+				Type: game.EventTypeMove,
+				Data: game.EventMove{
+					UnitID:    world.MyID,
+					Direction: game.DirectionDown,
+				},
+			})
+
+		return nil
+	}
+
+	if world.Units[world.MyID].Action == game.ActionRun {
+		c.WriteJSON(game.Event{
+			Type: game.EventTypeIdle,
+			Data: game.EventIdle{
+				UnitID: world.MyID,
+			},
+		})
+	}
 	return nil
 }
 
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
-	img, _, _ := ebitenutil.NewImageFromFile("frames/big_demon_idle_anim_f0.png")
+	frame++
+	// img, _, _ := ebitenutil.NewImageFromFile("frames/big_demon_idle_anim_f0.png")
 
-	screen.DrawImage(img, nil)
-	// Write your game's rendering.
+	// screen.DrawImage(img, nil)
+
+	unitList := []*game.Unit{}
+	for _, unit := range world.Units {
+		unitList = append(unitList, unit)
+	}
+	sort.Slice(unitList, func(i, j int) bool {
+		return unitList[i].Y < unitList[j].Y
+	})
+	for _, unit := range unitList {
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(unit.X, unit.Y)
+		spriteIndex := (frame/12 + unit.Frame) % 4
+		img, _, _ = ebitenutil.NewImageFromFile("frames/" +
+			unit.SpriteName + "_" + unit.Action + "_anim_f" + strconv.Itoa(spriteIndex) + ".png")
+		screen.DrawImage(img, op)
+
+	}
+
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
@@ -35,7 +129,24 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 }
 
 func main() {
+	var err error
+	c, _, err = websocket.DefaultDialer.Dial("ws://127.0.0.1:8080/ws", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	go func(c *websocket.Conn) {
+		defer c.Close()
+
+		for {
+			var event game.Event
+			c.ReadJSON(&event)
+			world.HandleEvent(&event)
+		}
+	}(c)
+
 	game := &Game{}
+
 	// Specify the window size as you like. Here, a doubled size is specified.
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Your game's title")
