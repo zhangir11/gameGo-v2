c7b19b0867f25b73737888a8dab92d62f88c9625
Arman123 <Arman123>
2022-02-14 21:09:33 +0600

    add server logic

---
+++ server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"rpg/game"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	world := &game.World{
+		IsServer: true,
+		Units:    game.Units{},
+	}
+	hub := newHub()
+	go hub.run()
+	r := gin.New()
+	r.GET("/ws", func(hub *Hub, world *game.World) gin.HandlerFunc {
+		return gin.HandlerFunc(func(c *gin.Context) {
+			serveWs(hub, world, c.Writer, c.Request)
+		})
+	}(hub, world))
+	r.Run(":8080")
+	fmt.Println(r)
+}
